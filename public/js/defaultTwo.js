ac7a4baeb808e2172ca7fef6d0e8f26cd1a02b09
kevin-england <kevinengland5051@gmail.com>
2015-12-29 13:19:41 -0800

    Continued working on content additions to all new story objects created by JS that will be used as additional pathways for the stories, #2

--- public/js/defaultTwo.js
+++ public/js/defaultTwo.js
@@ -1,17 +1,31 @@
 var stories = { stories: [
   { 
     header: 'An egg named Elvie begins to roll...', 
-    content: 'Elvie rolls down a hillside of the Orthian planes. Swerving from right to left, he comes to a split in his path. Which way will Elvie go?',
+    content: 'Elvie rolls down a hillside shaped like a woman in a dress here on the spanning Orthian planes. Swerving from right to left, he comes to a split in his path. Which way will Elvie go?',
     image: 'images/egg.jpg',
     button: 'Continue down the hill',
     buttonOne: 'Go towards an unexpected alleyway'
   },
   { 
-    header: 'A hare named Gervis has just begun a race...', 
+    header: 'A hare named Gervis has started a race...', 
     content: 'The young hare has already gained a substantial lead and must decide what his next move will be. What is Gervis going to do?',
     image: 'images/hare.jpg',
     button: 'Take a quick break',
     buttonOne: 'Use shortcut through random rabbit hole'
+  },
+  { 
+    header: 'Elvie continues on down the hill...', 
+    content: 'Elvie rolls down the hill and sees two Orthian unicorns parting ways. One is a big white Steed and the other was a black horn with a brown nose. Elvie wants to follow one, which unicorn will he follow?',
+    image: 'images/eggOne.jpg',
+    button: 'White Unicorn',
+    buttonOne: 'Black Unicorn'
+  },
+  { 
+    header: 'Gervis goes through the rabbit hole...', 
+    content: 'As Gervis enters the hole, he can see the light from the other side of his shortcut. The problem is the owner has just come out with an Orthian pistol shouting at him to get off his property! Uh oh, what will Gervis do now?',
+    image: 'images/hareOne.jpg',
+    button: 'Run for the light!',
+    buttonOne: 'Run back!'
   }
 ]};
 
