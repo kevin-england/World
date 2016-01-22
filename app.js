2e3365bbdccc915a3852d0cf97246436d138b489
kevin-england <kevinengland5051@gmail.com>
2016-01-22 14:29:12 -0800

    Reworking all JavaScript object content to be within a JSON file so all data requests will be pulled from the back end

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var story = require('./story.json')
 
 app.use('/', express.static('public')); 
 
--- public/js/story.js
+++ public/js/story.js
@@ -1,1 +1,17 @@
-//Plan to pull all object information from backend and send to front end
+//get character choices on the page and on click ask the server for the story
+//clear out holders
+//create new elements
+//append to holders
+
+for (var i = 0; i < ./story.json.choices.length; i++) {
+  if (e.target == 'BUTTON') {
+    var choices = document.getElementById('button-holder');
+    choices.addEventListener('click', function() {
+      new xhrHTTPRequest = ''
+      choices.getAttribute('data-choices')
+    var newChoices = getAttribute('data-choices')
+    newChoices = document.getElementById('button-holder');
+    newChoices.style.display = 'block';
+    }, false);
+  };
+}
