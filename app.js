0c48dfbc2636e87dbfdd5445405a42f54ba22180
kevin-england <kevinengland5051@gmail.com>
2016-02-05 15:23:02 -0800

    Code clean up

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
 var express = require('express');
 var app = express();
 var story = require('./story.json')
+var request = require('request');
 
 app.use('/', express.static('public')); 
-
 
 //get character choices on the page and on click ask the server for the story
 //clear out holders
 //create new elements
 //append to holders
 
-var xhr = new XMLHttpRequest();
-xhr.onload = function () {
-  xhr.addEventListener('click', function(req, res) {
-    if (e.target == 'BUTTON') { 
-      var responseObject = JSON.parse(xhr.responseText);
-      xhr.open('GET', './story.json', true);
-      xhr.send(story.id[0]);
-      console.log(story.id[0]);
+var http = function() {
+  http.createServer(function (req, resp) {
+    if (req.url === './story.json') {
+      var firstChapter = request(story[0]);
+      req.pipe(firstChapter);
+      firstChapter.pipe(resp);
+      firstChapter.style.display = 'block';
     }
-    else {
-      var responseObject = JSON.parse(xhr.responseText);
-      xhr.open('GET', './story.json', true);
-      xhr.send(story.id[5]);
-      console.log(story.id[5]);
-    };
-  },false);
-}
+  })
+};
+
 
 app.listen(1337);
 console.log('This server is now live!');
